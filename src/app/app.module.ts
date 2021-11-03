6d26f1fbca293d0b255fb9a3df4a50d68ed7c73a
Alejandra Maria Del Carmen Sanchez Acosta <alejandramariadelcarmensanchezacosta@MacBook-Air-de-Alejandra.local>
2021-11-02 21:22:18 -0600

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './pages/login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ClientsComponent } from './pages/clients/clients.component';
+import { UsersComponent } from './pages/users/users.component';
+import { QuoteComponent } from './pages/quote/quote.component';
+import { ClientComponent } from './pages/client/client.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    ClientsComponent,
+    UsersComponent,
+    QuoteComponent,
+    ClientComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
